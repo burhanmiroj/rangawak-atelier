08d1bd95945ba0cf598116c7647f3efa75862362
Burhanudin Miroj <mirojburhanudin2@gmail.com>
2022-07-12 20:57:02 +0700

    :rocket: Update

---
+++ asset/js/back-to-top-btn.js
@@ -0,0 +1,23 @@
+const backToTopBTN = document.createElement("div");
+backToTopBTN.innerHTML = `<button class="w-16 h-16 animate-bounce transition-all duration-500 flex justify-center items-center opacity-0 fixed bottom-5 left-1/2 transform -translate-x-1/2 z-20">
+    <span class="iconify text-2xl" data-icon="bi:chevron-up"></span>
+</button>`;
+backToTopBTN.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+});
+
+window.addEventListener('scroll', function() {
+    const windowPosition = this.pageYOffset
+    const btnToTop = backToTopBTN.querySelector('button')
+
+    if(windowPosition > 200) {
+        btnToTop.classList.replace('opacity-0', 'opacity-1')
+    } else {
+        btnToTop.classList.replace('opacity-1', 'opacity-0')
+    } 
+})
+
+document.body.append(backToTopBTN);
