5e7636b47e31d7ef257714c374246f9832f22e57
Burhanudin Miroj <mirojburhanudin2@gmail.com>
2022-07-13 07:19:28 +0700

    :rocket: Update get in touch

--- asset/js/nav.js
+++ asset/js/nav.js
@@ -3,14 +3,25 @@ const mobileMenu = document.querySelector('#menu-dropdown')
 const navbar = document.querySelector('nav')
 const mobileMenuLink = document.querySelectorAll('#menu-dropdown li a')
 const searchProduct = document.querySelectorAll('#search_product')
+const headerLogo = navbar.querySelector('img')
 
 mobileMenuBtn.addEventListener('click', function() {
     mobileMenu.classList.remove('-translate-y-full')
 })
+// DARK CONTENT NAVBAR
+if (navbar.classList.contains('dark-content')) {        
+    headerLogo.setAttribute('src', '/asset/logo-black.png')
+
+    searchProduct.forEach(search => {
+        search.classList.replace('border-white', 'border-black')
+        search.classList.replace('placeholder-white', 'placeholder-black')
+        search.classList.replace('hover:placeholder-slate-100', 'hover:placeholder-slate-700')
+        search.classList.replace('text-white', 'text-black')
+    })
+}
 
 window.addEventListener('scroll', function() {
     const scrollPosition = this.pageYOffset
-    const headerLogo = navbar.querySelector('img')
 
     if(scrollPosition > navbar.clientHeight * 2) {
         const navbarChildrenWrapper = navbar.querySelector('div')
