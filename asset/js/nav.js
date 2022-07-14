33724f4b953d4d2b3f00fa040fad952a638c5c8c
Burhanudin Miroj <mirojburhanudin2@gmail.com>
2022-07-14 09:17:07 +0700

    :rocket: Update

--- asset/js/nav.js
+++ asset/js/nav.js
@@ -20,77 +20,77 @@ if (navbar.classList.contains('dark-content')) {
     })
 }
 
-window.addEventListener('scroll', function() {
-    const scrollPosition = this.pageYOffset
+// window.addEventListener('scroll', function() {
+//     const scrollPosition = this.pageYOffset
 
-    if(scrollPosition > navbar.clientHeight * 2) {
-        const navbarChildrenWrapper = navbar.querySelector('div')
+//     if(scrollPosition > navbar.clientHeight * 2) {
+//         const navbarChildrenWrapper = navbar.querySelector('div')
         
-        mobileMenu.classList.add('pt-5')
-        navbar.classList.add('bg-black')
-        navbarChildrenWrapper.classList.replace('h-20', 'h-16')
+//         mobileMenu.classList.add('pt-5')
+//         navbar.classList.add('bg-black')
+//         navbarChildrenWrapper.classList.replace('h-20', 'h-16')
 
-        // 
-        mobileMenuBtn.classList.add('text-white')
+//         // 
+//         mobileMenuBtn.classList.add('text-white')
         
-        // DARK CONTENT NAVBAR
-        if (navbar.classList.contains('dark-content')) {        
-            headerLogo.setAttribute('src', '/asset/logo.png')
-            mobileMenuBtn.classList.replace('text-black', 'text-slate-100')
+//         // DARK CONTENT NAVBAR
+//         if (navbar.classList.contains('dark-content')) {        
+//             headerLogo.setAttribute('src', '/asset/logo.png')
+//             mobileMenuBtn.classList.replace('text-black', 'text-slate-100')
 
-            searchProduct.forEach(search => {
-                search.classList.replace('border-black', 'border-white')
-                search.classList.replace('placeholder-black', 'placeholder-white')
-                search.classList.replace('text-black', 'text-white')
-            })
-        }
-    } else {
-        const navbarChildrenWrapper = navbar.querySelector('div')
+//             searchProduct.forEach(search => {
+//                 search.classList.replace('border-black', 'border-white')
+//                 search.classList.replace('placeholder-black', 'placeholder-white')
+//                 search.classList.replace('text-black', 'text-white')
+//             })
+//         }
+//     } else {
+//         const navbarChildrenWrapper = navbar.querySelector('div')
         
-        mobileMenu.classList.remove('pt-5')
-        navbar.classList.remove('bg-black')
-        navbarChildrenWrapper.classList.replace('h-16', 'h-20')
+//         mobileMenu.classList.remove('pt-5')
+//         navbar.classList.remove('bg-black')
+//         navbarChildrenWrapper.classList.replace('h-16', 'h-20')
 
-        // DARK CONTENT NAVBAR
-        if (navbar.classList.contains('dark-content')) {        
+//         // DARK CONTENT NAVBAR
+//         if (navbar.classList.contains('dark-content')) {        
 
-            mobileMenuBtn.classList.remove('text-white')
+//             mobileMenuBtn.classList.remove('text-white')
 
-            headerLogo.setAttribute('src', '/asset/logo-black.png')
-            mobileMenuBtn.classList.replace('text-slate-100', 'text-black')
+//             headerLogo.setAttribute('src', '/asset/logo-black.png')
+//             mobileMenuBtn.classList.replace('text-slate-100', 'text-black')
 
-            searchProduct.forEach(search => {
-                search.classList.replace('border-white', 'border-black')
-                search.classList.replace('placeholder-white', 'placeholder-black')
-                search.classList.replace('text-white', 'text-black')
-            })
-        }
-    }
+//             searchProduct.forEach(search => {
+//                 search.classList.replace('border-white', 'border-black')
+//                 search.classList.replace('placeholder-white', 'placeholder-black')
+//                 search.classList.replace('text-white', 'text-black')
+//             })
+//         }
+//     }
     
-    if(scrollPosition > 500) {
-        mobileMenuLink.forEach(link => {
-            link.classList.add('text-black')
-        })
-        if(navbar.classList.contains(!('black-content'))) {
-            searchProduct.forEach(search => {
-                search.classList.replace('border-white', 'border-black')
-                search.classList.replace('placeholder-white', 'placeholder-black')
-                search.classList.replace('text-white', 'text-black')
-                search.classList.add('lg:border-white')
-                search.classList.add('lg:placeholder-white')
-                search.classList.add('lg:text-white')
-            })
-        }
-    } else {
-        mobileMenuLink.forEach(link => {
-            link.classList.remove('text-black')
-        })
-        if(navbar.classList.contains(!('black-content'))) {
-            searchProduct.forEach(search => {
-                search.classList.replace('border-black', 'border-white')
-                search.classList.replace('placeholder-black', 'placeholder-white')
-                search.classList.replace('text-black', 'text-white')
-            })
-        }
-    }
-})
+//     if(scrollPosition > 500) {
+//         mobileMenuLink.forEach(link => {
+//             link.classList.add('text-black')
+//         })
+//         if(navbar.classList.contains(!('black-content'))) {
+//             searchProduct.forEach(search => {
+//                 search.classList.replace('border-white', 'border-black')
+//                 search.classList.replace('placeholder-white', 'placeholder-black')
+//                 search.classList.replace('text-white', 'text-black')
+//                 search.classList.add('lg:border-white')
+//                 search.classList.add('lg:placeholder-white')
+//                 search.classList.add('lg:text-white')
+//             })
+//         }
+//     } else {
+//         mobileMenuLink.forEach(link => {
+//             link.classList.remove('text-black')
+//         })
+//         if(navbar.classList.contains(!('black-content'))) {
+//             searchProduct.forEach(search => {
+//                 search.classList.replace('border-black', 'border-white')
+//                 search.classList.replace('placeholder-black', 'placeholder-white')
+//                 search.classList.replace('text-black', 'text-white')
+//             })
+//         }
+//     }
+// })
